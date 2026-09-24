566bbebe0a64165501c5d508a8128acf006b8129
yuhan8954 <yuhan8954@gmail.com>
2026-09-24 18:25:34 +0900

    chore: bootstrap Flashback 1710 project

--- src/main/java/me/yuhan8954/flashback/Flashback1710.java
+++ src/main/java/me/yuhan8954/flashback/Flashback1710.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -12,39 +11,33 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
 @Mod(
     modid = Flashback1710.MODID,
-    version = Tags.VERSION,
+    version = Flashback1710.VERSION,
     name = "Flashback 1710",
-    acceptedMinecraftVersions = "[1.7.10]")
+    acceptedMinecraftVersions = "[1.7.10]",
+    acceptableRemoteVersions = "*")
 public class Flashback1710 {
 
     public static final String MODID = "flashback1710";
     public static final Logger LOG = LogManager.getLogger(MODID);
-
-    @SidedProxy(clientSide = "me.yuhan8954.flashback.ClientProxy", serverSide = "me.yuhan8954.flashback.CommonProxy")
-    public static CommonProxy proxy;
+    public static final String VERSION = Tags.VERSION;
 
     @Mod.EventHandler
-    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
-    // GameRegistry." (Remove if not needed)
     public void preInit(FMLPreInitializationEvent event) {
-        proxy.preInit(event);
+
     }
 
     @Mod.EventHandler
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
     public void init(FMLInitializationEvent event) {
-        proxy.init(event);
+
     }
 
     @Mod.EventHandler
-    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
     public void postInit(FMLPostInitializationEvent event) {
-        proxy.postInit(event);
+
     }
 
     @Mod.EventHandler
-    // register server commands in this event handler (Remove if not needed)
     public void serverStarting(FMLServerStartingEvent event) {
-        proxy.serverStarting(event);
+
     }
 }
---
+++ src/main/java/me/yuhan8954/flashback/mixin/MixinNetworkManager.java
@@ -0,0 +1,9 @@
+package me.yuhan8954.flashback.mixin;
+
+import net.minecraft.network.NetworkManager;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(NetworkManager.class)
+public abstract class MixinNetworkManager {
+
+}
