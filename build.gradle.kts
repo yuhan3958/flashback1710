c6d543a5888c0dcc6ae77925e2f8d34b073ced1e
yuhan8954 <yuhan8954@gmail.com>
2026-09-24 21:24:40 +0900

    build: configure Kotlin and mixin support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,21 @@
 
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
+    kotlin("jvm")
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
+        )
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
 }
