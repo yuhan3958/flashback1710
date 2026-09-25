02cdb629b2d4f600c01c6087307ccc079ca98a32
YUHAN KIM <89955275+yuhan3958@users.noreply.github.com>
2026-09-26 00:12:00 +0900

    style: format chunk mutation mixin

--- src/main/java/me/yuhan8954/flashback/mixin/MixinChunk.java
+++ src/main/java/me/yuhan8954/flashback/mixin/MixinChunk.java
@@ -1,75 +1,41 @@
 package me.yuhan8954.flashback.mixin;
 
-import me.yuhan8954.flashback.replay.ReplayChunkMutationHooks;
 import net.minecraft.block.Block;
 import net.minecraft.world.chunk.Chunk;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import me.yuhan8954.flashback.replay.ReplayChunkMutationHooks;
+
 @Mixin(Chunk.class)
 public abstract class MixinChunk {
 
     @Inject(method = "func_150807_a", at = @At("HEAD"))
-    private void flashback1710$beforeSetBlock(
-        int x,
-        int y,
-        int z,
-        Block block,
-        int metadata,
-        CallbackInfoReturnable<Boolean> cir
-    ) {
-        ReplayChunkMutationHooks.beforeBlockChange(
-            (Chunk) (Object) this,
-            x,
-            y,
-            z
-        );
+    private void flashback1710$beforeSetBlock(int x, int y, int z, Block block, int metadata,
+        CallbackInfoReturnable<Boolean> cir) {
+        ReplayChunkMutationHooks.beforeBlockChange((Chunk) (Object) this, x, y, z);
     }
 
     @Inject(method = "func_150807_a", at = @At("RETURN"))
-    private void flashback1710$afterSetBlock(
-        int x,
-        int y,
-        int z,
-        Block block,
-        int metadata,
-        CallbackInfoReturnable<Boolean> cir
-    ) {
-        ReplayChunkMutationHooks.afterBlockChange(
-            cir.getReturnValue()
-        );
+    private void flashback1710$afterSetBlock(int x, int y, int z, Block block, int metadata,
+        CallbackInfoReturnable<Boolean> cir) {
+        ReplayChunkMutationHooks.afterBlockChange(cir.getReturnValue());
     }
 
     @Inject(method = "setBlockMetadata", at = @At("HEAD"))
-    private void flashback1710$beforeSetBlockMetadata(
-        int x,
-        int y,
-        int z,
-        int metadata,
-        CallbackInfoReturnable<Boolean> cir
-    ) {
-        ReplayChunkMutationHooks.beforeBlockChange(
-            (Chunk) (Object) this,
-            x,
-            y,
-            z
-        );
+    private void flashback1710$beforeSetBlockMetadata(int x, int y, int z, int metadata,
+        CallbackInfoReturnable<Boolean> cir) {
+        ReplayChunkMutationHooks.beforeBlockChange((Chunk) (Object) this, x, y, z);
     }
 
     @Inject(method = "setBlockMetadata", at = @At("RETURN"))
-    private void flashback1710$afterSetBlockMetadata(
-        int x,
-        int y,
-        int z,
-        int metadata,
-        CallbackInfoReturnable<Boolean> cir
-    ) {
-        ReplayChunkMutationHooks.afterBlockChange(
-            cir.getReturnValue()
-        );
+    private void flashback1710$afterSetBlockMetadata(int x, int y, int z, int metadata,
+        CallbackInfoReturnable<Boolean> cir) {
+        ReplayChunkMutationHooks.afterBlockChange(cir.getReturnValue());
     }
 
     @Inject(method = "fillChunk", at = @At("HEAD"))
@@ -78,11 +44,8 @@ public abstract class MixinChunk {
         int primaryBitMask,
         int addBitMask,
         boolean includeBiome,
-        CallbackInfo ci
-    ) {
-        ReplayChunkMutationHooks.beforeFillChunk(
-            (Chunk) (Object) this
-        );
+        CallbackInfo ci) {
+        ReplayChunkMutationHooks.beforeFillChunk((Chunk) (Object) this);
     }
 
     @Inject(method = "fillChunk", at = @At("RETURN"))
@@ -91,8 +54,7 @@ public abstract class MixinChunk {
         int primaryBitMask,
         int addBitMask,
         boolean includeBiome,
-        CallbackInfo ci
-    ) {
+        CallbackInfo ci) {
         ReplayChunkMutationHooks.afterFillChunk();
     }
 }
